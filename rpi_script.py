e1507ca4a5213ff82d32b5bbb8ab61c4c78d05b7
Josh Davey <jdavey1996@gmail.com>
2016-11-15 02:24:55 +0000

    Raspberry Pi PIR test
    Script to testing the PIR sensor detecting motion. This is currently on
    a loop but in the future this will be set up as an interrupt.

--- rpi_script.py
+++ rpi_script.py
@@ -1,16 +1,35 @@
-//Setup
+#Import libraries to enable waiting and GPIO access.
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
+#Set mode to BOARD (Pins are referred to by their pin number, compared to BCM where they're referred to by GPIO number).
+GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-GPIO.setup(18,GPIO.OUT)
+#Set up PIR sensor on pin 16.
+GPIO.setup(16,GPIO.IN)
+#Set up LED on pin 12.
+GPIO.setup(12,GPIO.OUT)
 
-//Simple loop turning an LED on/off.
+#Initially set LED to off before starting to detect motion.
+GPIO.output(12,GPIO.LOW)
+
+#Sleep for 4 second.
+time.sleep(4)
+
+#Continuous loop to check if motion is detected.
 num = 0
-while num< 1):
-	//Sets pin controlling LED to high.
-	GPIO.output(18,GPIO.HIGH)
+while num< 1:
+	#Reads PIR input into variable.
+	i=GPIO.input(16)
+	#Code to execute when motion is not detected.
+	if i==0:
+		print "NO MOTION DETECTED"
+		#Sets pin controlling LED to low.
+		GPIO.output(12,GPIO.LOW)
+	#Code to execute when motion is detected.
+	elif i==1:
+		print "MOTION DETECTED"
+		#Sets pin controlling LED to high.
+		GPIO.output(12,GPIO.HIGH)
+
+	#Sleep for 2 seconds.
 	time.sleep(2)
-	//Sets pin controlling LED to low.
-	GPIO.output(18,GPIO.LOW)
-   	time.sleep(2)
