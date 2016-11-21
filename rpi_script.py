7f3f500dc28154c112ad551b2b3dafaec39c86a5
Josh Davey <jdavey1996@gmail.com>
2016-11-21 21:16:15 +0000

    Motion sensing on/off
    Motion sensing interrupts can now be turned on and off by sending
    corresponding parameters to the raspberry pi web server (local network
    only). Once motion is detected, an LED lights up, then turns off once it
    stops detecting.

--- rpi_script.py
+++ rpi_script.py
@@ -1,5 +1,6 @@
 import web
 import RPi.GPIO as GPIO
+import time
 
 urls = (
     '/', 'index'
@@ -9,20 +10,35 @@ urls = (
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
 LED_PIN = 12
+PIR_PIN = 16
 #Set up LED on pin 12.
 GPIO.setup(LED_PIN,GPIO.OUT)
+#Set up PIR sensor on pin 16.
+GPIO.setup(PIR_PIN,GPIO.IN)
 
+def MOTION(PIR_PIN):
+	if GPIO.input(PIR_PIN):
+		print "Motion detected"
+		GPIO.output(LED_PIN,GPIO.HIGH)
+	else:
+		print "Motion no longer detected"
+		GPIO.output(LED_PIN,GPIO.LOW)
+
+time.sleep(4)
 
 class index:
-	def GET(self):
-	    user_data = web.input()
-	    if user_data.userin == 'on':
-	        GPIO.output(LED_PIN,GPIO.HIGH)
-            return "ON!"
-        elif user_data.userin == 'off':
-            GPIO.output(LED_PIN,GPIO.LOW)
-		    return "OFF!"
+        def GET(self):
+                user_data = web.input()
+                if user_data.userin == 'on':
+                        print "turned on"
+                        GPIO.add_event_detect(PIR_PIN, GPIO.BOTH, callback = MOTION)
+                        return "ON!"
+                elif user_data.userin == 'off':
+                        print "turned off"
+                        GPIO.remove_event_detect(PIR_PIN)
+                        GPIO.output(LED_PIN,GPIO.LOW)
+                        return "OFF!"
 
 if __name__ == "__main__":
-    app = web.application(urls, globals())
-    app.run()
+        app = web.application(urls, globals())
+        app.run()
