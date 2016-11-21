80bfc54dea4814b4c391c0ff4a6be3ddb794fb73
Josh Davey <jdavey1996@gmail.com>
2016-11-21 15:58:55 +0000

    Raspberry Pi Web server
    Set up the raspberry Pi webserver, removing the PIR aspect for now as it
    needs testing further. This currently sets up a local server accessible
    via devices on the network. Parameters 'on' or 'off' can be passed
    through via a url, which enables and disables an LED.
    The next step is to integrate the PIR sensor back in, send notifcations
    once motion has been detected, and make the server accessible via the
    internet (not just local network) by port forwarding.

--- rpi_script.py
+++ rpi_script.py
@@ -1,35 +1,28 @@
-#Import libraries to enable waiting and GPIO access.
+import web
 import RPi.GPIO as GPIO
-import time
+
+urls = (
+    '/', 'index'
+)
+
 #Set mode to BOARD (Pins are referred to by their pin number, compared to BCM where they're referred to by GPIO number).
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-#Set up PIR sensor on pin 16.
-GPIO.setup(16,GPIO.IN)
+LED_PIN = 12
 #Set up LED on pin 12.
-GPIO.setup(12,GPIO.OUT)
+GPIO.setup(LED_PIN,GPIO.OUT)
 
-#Initially set LED to off before starting to detect motion.
-GPIO.output(12,GPIO.LOW)
 
-#Sleep for 4 second.
-time.sleep(4)
+class index:
+	def GET(self):
+	    user_data = web.input()
+	    if user_data.userin == 'on':
+	        GPIO.output(LED_PIN,GPIO.HIGH)
+            return "ON!"
+        elif user_data.userin == 'off':
+            GPIO.output(LED_PIN,GPIO.LOW)
+		    return "OFF!"
 
-#Continuous loop to check if motion is detected.
-num = 0
-while num< 1:
-	#Reads PIR input into variable.
-	i=GPIO.input(16)
-	#Code to execute when motion is not detected.
-	if i==0:
-		print "NO MOTION DETECTED"
-		#Sets pin controlling LED to low.
-		GPIO.output(12,GPIO.LOW)
-	#Code to execute when motion is detected.
-	elif i==1:
-		print "MOTION DETECTED"
-		#Sets pin controlling LED to high.
-		GPIO.output(12,GPIO.HIGH)
-
-	#Sleep for 2 seconds.
-	time.sleep(2)
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
