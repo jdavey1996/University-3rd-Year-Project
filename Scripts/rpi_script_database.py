5f505a06d1809dd7e995e90b17da45e1065b0ebd
Josh Davey <jdavey1996@gmail.com>
2016-12-29 19:50:57 +0000

    SQLite integration & Get detection history
    Updated the RPi scripts. This allows information about each detection to
    be stored in a database and sent via HTTP Get requests to requesting
    users in JSON format.

---
+++ Scripts/rpi_script_database.py
@@ -0,0 +1,39 @@
+import sqlite3
+import time
+import json
+
+class Database:
+    @staticmethod
+    def createDb():
+        db_con = sqlite3.connect('3rdyearproject.db')
+        db_cur = db_con.cursor()
+        #Create database if doesn't already exist.
+        db_cur.execute('''CREATE TABLE IF NOT EXISTS tbl_detections(detection_num INTEGER PRIMARY KEY, detection_date text, detection_time text)''')
+        db_con.commit()
+        db_con.close()
+
+    @staticmethod
+    def insertDb():
+        db_con = sqlite3.connect('3rdyearproject.db')
+        db_cur = db_con.cursor()
+        now_date = (time.strftime("%d/%m/%Y"))
+        now_time = (time.strftime("%H:%M:%S"))
+        db_cur.execute("INSERT INTO tbl_detections (detection_date, detection_time) VALUES (?, ?)",(now_date, now_time))
+        db_con.commit()
+        db_con.close()
+
+    @staticmethod
+    def getDataDb():
+        db_con = sqlite3.connect('3rdyearproject.db')
+        db_cur = db_con.cursor()
+        db_cur.execute('SELECT * FROM tbl_detections ORDER BY detection_num')
+
+        detectionsList = []
+        for row in db_cur:
+            id = row[0]
+            date = row[1]
+            time = row[2]
+            detectionsList.append({'id': id, 'date' : date, 'time': time})
+
+        db_con.close()
+        return json.dumps(detectionsList)
--- Scripts/rpi_script_main.py
+++ Scripts/rpi_script_main.py
@@ -3,9 +3,14 @@ import requests
 import RPi.GPIO as GPIO
 import time
 import json
+import sqlite3
+from rpi_script_database import Database
+
+Database.createDb()
 
 urls = (
-    '/', 'index'
+    '/control', 'control',
+    '/history', 'history'
 )
 
 #Set mode to BOARD (Pins are referred to by their pin number, compared to BCM where they're referred to by GPIO number).
@@ -19,11 +24,13 @@ GPIO.setup(LED_PIN,GPIO.OUT)
 #Set up PIR sensor on pin 16.
 GPIO.setup(PIR_PIN,GPIO.IN)
 
-FCM_API_KEY="____________________"
+FCM_API_KEY="AAAAayrfxpk:APA91bGkKVon_NZHLtiyHq4QSacUiNk_TDr9l7DTmsCdEry0jnvddnJwhrUdhygCjwpLaa9cYpCF7CzXrW86EuWlaiE1Th9T5mNvb1FR60qT_i_KoVP9JyV3gdWHB1w7lyX8YOJFiCwaiqfc7G_2L2fCJmvneAnhQw"
+
+
 
 def NOTIFICATION(FCM_API_KEY):
     	data={
-        	"to" : "_____device id____",
+        	"to" : "eEoKl2EHjpc:APA91bGHQJNL7s-WJ_ZDn5Ud9CVwUbnaz4TxuySRLF_6sZt8w8d0x5xMpedwx-itG2Br3VUji65ossG-b3gR_Ehe14rTA7cYmIdzFmrYwbvoxC-s-medajSAZYHjKi1oPeKa95GBatj8",
         	"data" : {
             	"notification" : "motion detected"
             	}
@@ -36,14 +43,17 @@ def NOTIFICATION(FCM_API_KEY):
 
 def MOTION(PIR_PIN):
 	if GPIO.input(PIR_PIN):
-		print "Motion detected"
+		#Set LED to on.
 		GPIO.output(LED_PIN,GPIO.HIGH)
+        	#Send notification.
         	NOTIFICATION(FCM_API_KEY)
+        	#Add detection to database.
+        	Database.insertDb()
 	else:
 		print "Motion no longer detected"
 		GPIO.output(LED_PIN,GPIO.LOW)
 
-class index:
+class control:
         def POST(self):
                 data = json.loads(web.data())
                 command = data["command"]
@@ -66,7 +76,10 @@ class index:
                         elif running == 0:
                             	return "002"
 
+class history:
+        def GET(self):
+                return Database.getDataDb()
+
 if __name__ == "__main__":
         app = web.application(urls, globals())
         app.run()
-
